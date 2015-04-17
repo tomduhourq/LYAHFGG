8b9ba230e48975f2f2f40a4b89618d846b1cc53a
Tomás <t.duhourq@gmail.com>
2015-04-17 00:24:00 -0300

    Indent

--- Ch2/baby.hs
+++ Ch2/baby.hs
@@ -4,10 +4,7 @@ doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 
 -- The 'else' part is mandatory in Haskell. since every expression MUST retunr something
-doubleSmallNumber x = if x > 100 
-					  then x 
-					  else 
-					  	doubleMe x
+doubleSmallNumber x = if x > 100 then x else doubleMe x
 
 -- Lists are constructed with head:tail operator. Strings are lists.
 -- To get an element out of a list, do list !! num, num starts by 0.
