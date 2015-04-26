2840649d7a99850386e0524db98dd69fc794d21d
Tomás <t.duhourq@gmail.com>
2015-04-25 22:34:09 -0300

    Add LastButOne + props

--- H99/Lists.hs
+++ H99/Lists.hs
@@ -9,3 +9,14 @@ myLast (_ : xs) = myLast xs
 prop_headRev xs = xs == [] || head (reverse xs) == myLast xs
 prop_uniqueElem x = myLast (x : []) == x
 	where types = x :: Int
+
+-- 2) Find the last but one element of a List
+lastButOne :: [a] -> a
+lastButOne [] = myLast []
+lastButOne xs = reverse xs !! 1
+
+prop_uniqueElems x y = lastButOne (x : y : []) == x
+	where types = (x :: Int, y :: Int)
+prop_mimic xs = xs == [] ||
+			    length xs == 1 || 
+			    reverse xs !! 1 == lastButOne xs
